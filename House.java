1eb103ab38fd9a848b33f1bc5d67aed61f8cbb51
tolentinoo <rtolentino@smith.edu>
2022-10-27 14:42:14 -0400

    In class work

--- House.java
+++ House.java
@@ -1,8 +1,17 @@
 /* This is a stub for the House class */
-public class House {
+import java.util.ArrayList;
+public class House extends Building {
 
-  public House() {
+  ArrayList <String> residents;
+  boolean hasDiningRoom;
+
+
+  public House(String name, String address, int nFloors, boolean hasDR) {
+    super( name, address, nFloors); //belongs to house we're making using the attributes of Building
+    this.hasDiningRoom = hasDR;
+    this.residents = new ArrayList<String>();
     System.out.println("You have built a house: 🏠");
+
   }
 
   public static void main(String[] args) {
