30cc4a4f41ae942cb244d64f191fb65c8d8b22f7
tolentinoo <rtolentino@smith.edu>
2022-10-31 23:49:33 -0400

    last 10/31

--- Library.java
+++ Library.java
@@ -11,15 +11,24 @@ public class Library extends Building {
       System.out.println("You have built a library: 📖");
     }
     public void addTitle(String title){
-
+      this.collection.put(title,true);
     }
     public String removeTitle(String title){
-      
+      this.collection.remove(title);
+      return title;
     } // return the title that we removed  
+    public void checkOut(String title){
+      
+    }
+    public void returnBook(String title){
+
+    }
   
     public static void main(String[] args) {
       Library FL = new Library ("Forbes ", "33 Green St Northampton, MA 01063", 4);
       System.out.println(FL);
+      FL.addTitle("New Story");
+      System.out.println(FL.collection);
     }
   
   }
