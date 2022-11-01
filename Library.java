b782dd92b7e40cbaee4666eb4fb9ea259f4f8afe
tolentinoo <rtolentino@smith.edu>
2022-10-31 21:46:52 -0400

    Library 2

--- Library.java
+++ Library.java
@@ -3,15 +3,23 @@ import java.util.Hashtable;
 
 /* This is a stub for the Library class */
 public class Library extends Building {
-  private Hashtable<String, Boolean> collection;
+  private  Hashtable<String, Boolean> collection;
 
-    public Library() {
-      super(getName(), getAddress(), getFloors())
+    public Library(String name, String address, int nFloors) {
+      super( name, address, nFloors);
+      this.collection = new Hashtable<String, Boolean> ();
       System.out.println("You have built a library: 📖");
     }
+    public void addTitle(String title){
+
+    }
+    public String removeTitle(String title){
+      
+    } // return the title that we removed  
   
     public static void main(String[] args) {
-      new Library();
+      Library FL = new Library ("Forbes ", "33 Green St Northampton, MA 01063", 4);
+      System.out.println(FL);
     }
   
   }
