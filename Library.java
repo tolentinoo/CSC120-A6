6f4354c8efa0b8553f85551cf09dfae707ba11e8
tolentinoo <rtolentino@smith.edu>
2022-10-31 21:13:27 -0400

    Library Started

--- Library.java
+++ Library.java
@@ -1,7 +1,12 @@
+import java.util.Hashtable;
+
+
 /* This is a stub for the Library class */
-public class Library {
+public class Library extends Building {
+  private Hashtable<String, Boolean> collection;
 
     public Library() {
+      super(getName(), getAddress(), getFloors())
       System.out.println("You have built a library: 📖");
     }
   
