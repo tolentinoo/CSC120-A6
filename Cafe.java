41e49df6d7caaac711387c7b6c165b2566456b75
tolentinoo <rtolentino@smith.edu>
2022-11-01 23:51:49 -0400

    cafe begun

--- Cafe.java
+++ Cafe.java
@@ -1,8 +1,24 @@
 /* This is a stub for the Cafe class */
-public class Cafe {
+public class Cafe extends Building {
 
-    public Cafe() {
+    private int nCoffeeOunces; // The number of ounces of coffee remaining in inventory
+    private int nSugarPackets; // The number of sugar packets remaining in inventory
+    private int nCreams; // The number of "splashes" of cream remaining in inventory
+    private int nCups; // The number of cups remaining in inventory
+
+    public Cafe (String name, String address, int nFloors){
+        super(name, address, nFloors);
+        this.nCoffeeOunces = 3 ;
+        this.nSugarPackets = 3;
+        this.nCreams = 3; 
+        this.nCups = 3;
+        
+
         System.out.println("You have built a cafe: ☕");
+    }
+
+    public void sellCoffee(int size, int nSugarPackets, int nCreams){
+        
     }
     
     public static void main(String[] args) {
