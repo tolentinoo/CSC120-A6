4a3d517bffebc85d43136ab663ec46d077e4cfaa
tolentinoo <rtolentino@smith.edu>
2022-11-01 23:22:27 -0400

    Library finished

--- Library.java
+++ Library.java
@@ -15,15 +15,25 @@ public class Library extends Building {
     }
     public String removeTitle(String title){
       this.collection.remove(title);
-      return title;
-    } // return the title that we removed  
+      return title; // return the title that we removed  
+    
+    } 
     public void checkOut(String title){
-      
+      this.collection.replace(title,true,false );
     }
     public void returnBook(String title){
-
+      this.collection.replace(title,false,true );
     }
-  
+    public boolean containsTitle(String title){
+      return this.collection.containsKey(title);
+
+    }// returns true if the title appears as a key in the Libary's collection, false otherwise
+    public boolean isAvailable(String title){ 
+      return this.collection.containsValue(title);
+    }// returns true if the title is currently available, false otherwise
+    public void printCollection(){
+
+    } // prints out the entire collection in an easy-to-read way (including checkout status)
     public static void main(String[] args) {
       Library FL = new Library ("Forbes ", "33 Green St Northampton, MA 01063", 4);
       System.out.println(FL);
